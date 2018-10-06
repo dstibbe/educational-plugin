c04a9a5859982b3d8bb8e153d5fc71bed900c6c2
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-06 16:21:49 +0300

    reuse java task checker

--- Edu-Java/src/com/jetbrains/edu/java/learning/stepik/alt/JHyperskillConfigurator.java
+++ Edu-Java/src/com/jetbrains/edu/java/learning/stepik/alt/JHyperskillConfigurator.java
@@ -1,13 +1,10 @@
 package com.jetbrains.edu.java.learning.stepik.alt;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.edu.java.learning.JTaskCheckerProvider;
 import com.jetbrains.edu.learning.checker.TaskCheckerProvider;
-import com.jetbrains.edu.learning.checker.gradle.GradleTaskCheckerProvider;
 import com.jetbrains.edu.learning.gradle.GradleConfiguratorBase;
 import com.jetbrains.edu.learning.gradle.GradleCourseBuilderBase;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class JHyperskillConfigurator extends GradleConfiguratorBase {
   private final JHyperskillCourseBuilder myCourseBuilder = new JHyperskillCourseBuilder();
@@ -27,12 +24,6 @@ public class JHyperskillConfigurator extends GradleConfiguratorBase {
   @NotNull
   @Override
   public TaskCheckerProvider getTaskCheckerProvider() {
-    return new GradleTaskCheckerProvider() {
-      @Nullable
-      @Override
-      protected String mainClassForFile(@NotNull Project project, @NotNull VirtualFile file) {
-        return null;
-      }
-    };
+    return new JTaskCheckerProvider();
   }
 }
