be0527b33eec3671e98ac8386d0f796426f9b322
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-09 16:39:20 +0200

    reuse base language

--- educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillStepikConnector.kt
+++ educational-core/src/com/jetbrains/edu/learning/stepik/alt/HyperskillStepikConnector.kt
@@ -9,13 +9,13 @@ import com.jetbrains.edu.learning.stepik.StepikConnector
 fun getLesson(lessonId: Int, language: Language, stages: List<HyperskillStage>): Lesson? {
   val lesson = StepikConnector.getLesson(lessonId)
   val stepIds = lesson.steps.map { stepId -> stepId.toString() }.toTypedArray()
-  val allStepSources = StepikConnector.getStepSources(stepIds, language.displayName)
+  val allStepSources = StepikConnector.getStepSources(stepIds, language.baseLanguage?.id)
   val tasks = StepikConnector.getTasks(language, stepIds, allStepSources)
   val convertedTasks = mutableListOf<EduTask>()
   for ((index, task) in tasks.withIndex()) {
     val stage = stages[index]
     val eduTask = EduTask(stage.title)
-    // TODO: extract as template
+    // TODO: extract as template or clone repository
     eduTask.addTaskFile(TaskFile("src/Task.java", ""))
     eduTask.descriptionText = task.descriptionText
     convertedTasks.add(eduTask)
