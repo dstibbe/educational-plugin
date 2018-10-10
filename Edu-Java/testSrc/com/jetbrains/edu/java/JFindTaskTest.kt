d532921b820dff518b86ff209eb52a16aacc5030
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-10 15:36:41 +0200

    unused imports

--- Edu-Java/testSrc/com/jetbrains/edu/java/JFindTaskTest.kt
+++ Edu-Java/testSrc/com/jetbrains/edu/java/JFindTaskTest.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.edu.java
 
-import com.jetbrains.edu.java.JCourseBuilder
 import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.FindTaskFileTestBase
 import com.jetbrains.edu.learning.gradle.JdkProjectSettings
