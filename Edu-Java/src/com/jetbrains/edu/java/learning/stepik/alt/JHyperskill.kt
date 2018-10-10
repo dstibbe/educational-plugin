e519bbb99f113170e0165ee902e28353987feaf0
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-10-10 15:15:36 +0200

    better exception handling

--- Edu-Java/src/com/jetbrains/edu/java/learning/stepik/alt/JHyperskill.kt
+++ Edu-Java/src/com/jetbrains/edu/java/learning/stepik/alt/JHyperskill.kt
@@ -5,5 +5,6 @@ import com.intellij.lang.Language
 import com.intellij.lang.java.JavaLanguage
 
 class JHyperskill : Language(JavaLanguage.INSTANCE, JHYPERSKILL_LANGUAGE), DependentLanguage {
-  override fun getDisplayName() = baseLanguage?.displayName ?: id
+  override fun getDisplayName() = baseLanguage?.displayName ?:
+                                  throw IllegalStateException("No base language found for hyperskill language")
 }
